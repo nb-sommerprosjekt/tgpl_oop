65434e9fb29e89e512751e3c1383e1b7772f2410
nb-sommerprosjekt <auwalsoe@gmail.com>
2018-02-02 09:07:25 +0100

    Minor bugfix in test_mlp.

--- run_test.py
+++ run_test.py
@@ -1,9 +1,9 @@
 import sys
-sys.path.append('/home/knutf/workspace/shipping_ready/tgpl_oop/nb_ml')
+sys.path.append('/home/ubuntu/PycharmProjects_saved/tgpl_w_oop/nb_ml')
 from nb_ml import test_MLP,test_CNN,test_fasttext
 
 
-# new_test=test_MLP.test_MLP("/home/knutf/workspace/shipping_ready/tgpl_oop/config/test_mlp.yml")
+# new_test=test_MLP.test_MLP("/home/knutf/workspace/shipping_ready/tgpl_oop/config/test_mlp_sveinb.yml")
 #
 # new_test.run_tests()
 
@@ -13,6 +13,10 @@ from nb_ml import test_MLP,test_CNN,test_fasttext
 # new_test.run_tests()
 
 
-new_test=test_fasttext.test_fasttext("/home/knutf/workspace/shipping_ready/tgpl_oop/config/test_fasttext.yml")
+#new_test=test_fasttext.test_fasttext("/home/knutf/workspace/shipping_ready/tgpl_oop/config/test_fasttext.yml")
 
-new_test.run_tests()
+#new_test.run_tests()
+
+sveinb_test = test_MLP.test_MLP("/home/ubuntu/PycharmProjects_saved/tgpl_w_oop/config/test_mlp_sveinb.yml")
+
+sveinb_test.run_tests()
